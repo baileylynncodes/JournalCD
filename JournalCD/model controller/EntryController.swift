f52ab850eeb761adb27fd71401eef9ccba0fe125
Karissa McDaris <rissydee@Karissas-MacBook-Air.local>
2018-12-06 11:30:07 -0700

    adds EntryController and CoreDataStack

--- JournalCD/model controller/EntryController.swift
+++ JournalCD/model controller/EntryController.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import CoreData
+
+class EntryController {
+    
+    static let sharedInstance = EntryController()
+    private init(){}
+    
+    func addEntryWith(title: String, body: String){
+        Entry(title: title, body: body)
+        saveToPersistanceStore()
+    }
+    
+    func update(entry: Entry, newTitle: String, newBody: String){
+        entry.title = newTitle
+        entry.body = newBody
+        saveToPersistanceStore()
+    }
+    
+    func delete(entry: Entry){
+        //works, but is not safe
+        //CoreDataStack.context.delete(entry)
+        
+        //better for multiple moc
+        entry.managedObjectContext?.delete(entry)
+        saveToPersistanceStore()
+    }
+    
+    func saveToPersistanceStore() {
+        do {
+            try CoreDataStack.context.save()
+        } catch {
+            print("\(error) \(#function)")
+        }
+    }
+}
